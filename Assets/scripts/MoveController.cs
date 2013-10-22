bb983193beb771ae45a3f44285c84c8e74e9fe31
mik403 <mik4041@gmail.com>
2013-10-22 23:50:38 +0300

    added jump force controlling

--- Assets/scripts/MoveController.cs
+++ Assets/scripts/MoveController.cs
@@ -3,12 +3,13 @@ using System.Collections;
 
 public class MoveController : MonoBehaviour {
 
-    public float speed = 10.0F;
-    public float jumpSpeed = 20.0F;
-    public float gravity = 10.0F;
+    public float speed = 6.0F;
+    public float jump_speed = 1.0F;
+    public float gravity = 14.0F;
  
 	private Vector3 move_direction = Vector3.zero;
-	
+	private int curr_jumping_time = 0;
+	private int max_jumping_time = 8; //max time value, when player can hold "jump" button and increase jump speed
 	CharacterAnimator character_animation;
 	
 	void Start () {
@@ -31,11 +32,21 @@ public class MoveController : MonoBehaviour {
 			move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, 0); //this is a 2D game, so only X-axis
         	move_direction = transform.TransformDirection(move_direction);
        	 	move_direction *= speed;
+			
+			curr_jumping_time = 0;
             if (Input.GetButton("Jump")) {
-                move_direction.y = jumpSpeed;
+                move_direction.y = jump_speed;
 			}
         } else {
+
 			move_direction.x = speed * Input.GetAxis("Horizontal"); //only horizontal move control while flying
+			//jumping force control
+			curr_jumping_time++;
+ 			if (Input.GetButton("Jump") && curr_jumping_time < max_jumping_time) {
+                	move_direction.y += jump_speed;	
+			} else {
+				curr_jumping_time = max_jumping_time; // if "jump" button was once upped, no more force increasing
+			}
 		}
 			
         move_direction.y -= gravity * Time.deltaTime;
